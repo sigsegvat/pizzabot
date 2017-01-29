3efb139b50232a2d488aec295fed54504adf8821
Matthias Kausl <sig@segv.at>
2017-01-29 11:43:04 +0100

    nvm

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 ./build/nvm.sh
-export NVM_DIR="/Users/mat/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 6.9
+export NVM_DIR="/Users/mat/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  && nvm install 6.9
