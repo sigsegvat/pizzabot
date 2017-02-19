03be42bb32382338b5cf736fc470c9664daeddb9
Matthias Kausl <sig@segv.at>
2017-02-19 11:14:19 +0100

    readded index

---
+++ src/index.js
@@ -0,0 +1,22 @@
+"use strict";
+let slack = require('@slack/client');
+let bot = require('./bot/pizzabot');
+
+let bot_token = process.env.slack_bot_token;
+
+let rtm = new slack.RtmClient(bot_token);
+let web = new slack.WebClient(bot_token);
+
+let pizzabot = new bot.Pizzabot(web);
+
+rtm.on(slack.CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  console.log(`Logged in as ${rtmStartData.self.name}`);
+});
+
+rtm.on(slack.RTM_EVENTS.MESSAGE, (msg) => {
+  console.log(msg);
+  pizzabot.onPizzaChannelMessage(msg);
+});
+
+rtm.start();
+console.log("started slackbot");
